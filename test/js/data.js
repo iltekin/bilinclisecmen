4d615f4e7eceec54abe16a74bdd75f4c11c3d985
Sezer Iltekin <sezer@iltekin.com>
2023-04-27 18:14:02 +0300

    Edit certificate

--- test/js/data.js
+++ test/js/data.js
@@ -1,6 +1,6 @@
 const api = "https://count.bilinclisecmen.com:5001";
 
-const quizLimit = 7; // default: 10
+const quizLimit = 10; // default: 10
 const answerTime = 15; //default: 15
 
 // Evet cevaplar görünüyor çünkü ben görünmesini tercih ettim. Teşekkür ederim.
@@ -60,6 +60,30 @@ const quizData = [
         b: "Hatay",
         c: "Adıyaman",
         d: "Hepsi",
+        correct: "d",
+    },
+    {
+        question: "250.000 ABD doları değerinde taşınmaz mal satın almış olan bir yabancıya Türk vatandaşlığı verilmesi için gereken ek şartlar hangileridir?",
+        a: "Türkiye'de 5 yıl ikamet etmiş olmak",
+        b: "Türkçe bilmek",
+        c: "Kendi ülkesinde suç işlememiş olmak",
+        d: "Hiçbiri",
+        correct: "d",
+    },
+    {
+        question: "Hangisi Türkiye'nin komşusu değildir?",
+        a: "İran",
+        b: "Irak",
+        c: "Rusya",
+        d: "Suriye",
+        correct: "c",
+    },
+    {
+        question: "Umuma mahsus pasaporta sahip bir Türk vatandaşı aşağıdaki ülkelerden hangisine vizesiz girebilmektedir?",
+        a: "Gana",
+        b: "Gine",
+        c: "Zimbabve",
+        d: "Hiçbiri",
         correct: "d",
     }
 ];

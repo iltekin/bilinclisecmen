32ed9b6e66e38f0d873248c0efc588631b2a543b
Sezer Iltekin <sezer@iltekin.com>
2023-05-01 15:51:01 +0300

    edit question number

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 const api = "https://count.bilinclisecmen.com:5001";
-const quizLimit = 1; // default: 10
+const quizLimit = 10; // default: 10
 const answerTime = 15; //default: 15
 
 // Bu dosyayı keşfedip sosyal medyada paylaşan kişi aşağıdakilerden hangisidir?
